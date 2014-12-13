3cc04274929893de39ec6f770d865392c38da8ef
Steve Hook <steve.hook@gmail.com>
2014-12-13 16:30:50 +0000

    Add a failing spec for the GET / API

---
+++ test/home.js
@@ -0,0 +1,12 @@
+var app = require('../app'),
+    request = require('supertest');
+
+describe('GET / API', function() {
+  describe('when requesting /', function() {
+    it('responds with success', function(done) {
+      request(app)
+        .get('/')
+        .expect(200, done);
+    });
+  });
+});
