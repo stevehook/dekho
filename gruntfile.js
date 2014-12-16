2f0f1054068801b685e66dae8e9cab12cdb58906
Steve Hook <steve.hook@gmail.com>
2014-12-16 17:22:19 +0000

    Configure grunt with watch plugin and jshint

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-cafe-mocha');
   grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-newer');
 
   grunt.initConfig({
     env: {
@@ -21,7 +24,45 @@ module.exports = function(grunt) {
           reporter: 'spec'
         },
       }
-    }
+    },
+    watch: {
+      js: {
+        files: ['./{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          // livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      livereload: {
+        // options: {
+        //   livereload: '<%= connect.options.livereload %>'
+        // },
+        files: [
+          'public/{,*/}*.html',
+          'public/styles/{,*/}*.css',
+          'public/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'gruntfile.js',
+          '{,*/}*.js'
+        ]
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/{,*/}*.js']
+      }
+    },
   });
   grunt.registerTask('test', [ 'env:test', 'cafemocha:test' ]);
+  grunt.registerTask('serve', [ 'env:development', 'watch' ]);
 };
