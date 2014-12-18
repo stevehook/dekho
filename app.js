af92574707b20b670529a9b72990c58db15171b9
Steve Hook <steve.hook@gmail.com>
2014-12-18 18:28:17 +0000

    Refactor authentication middleware into seperate file

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var express = require('express'),
     http = require('http'),
     bodyParser = require('body-parser'),
     decks = require('./routes/decks'),
+    authenticate = require('./lib/authenticate'),
     auth = require('./routes/auth'),
-    db = require('./models'),
     app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,30 +17,6 @@ app.use(function(req, res, next) {
   next();
 });
 app.set('port', process.env.PORT || 4000);
-
-var authenticate = function(request, response, next) {
-  var bearerHeader = request.headers.authorization;
-  if (typeof bearerHeader !== 'undefined') {
-    var bearer = bearerHeader.split(' ');
-    var bearerToken = bearer[1];
-    request.currentUser = undefined;
-    if (bearerToken) {
-      request.token = bearerToken;
-      db.Token.find({ where: { token: bearerToken } }).then(function(token) {
-        if (token) {
-          request.currentUser = token.user;
-          next();
-        } else {
-          response.sendStatus(403);
-        }
-      });
-    } else {
-      response.sendStatus(403);
-    }
-  } else {
-    response.sendStatus(403);
-  }
-};
 
 app.get('/', function(request, response) {
   response.status(200).json('OK');
