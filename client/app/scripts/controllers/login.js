751269bc3a5a2e4d76101113f8131c19c3c8c5cb
Steve Hook <steve.hook@gmail.com>
2015-01-01 17:16:02 +0000

    Adds client-side /login route and controller

---
+++ client/app/scripts/controllers/login.js
@@ -0,0 +1,29 @@
+
+'use strict';
+
+angular.module('dekho')
+  .constant('AUTH_EVENTS', {
+    loginSuccess: 'login-success',
+    loginFailed: 'login-failed',
+    logoutSuccess: 'logout-success',
+    logoutFailed: 'logout-failed'
+  })
+  .controller('LoginCtrl', function ($scope) {//$rootScope, $location, AUTH_EVENTS) {
+    $scope.credentials = {
+      email: ''
+    };
+    $scope.isLoggedIn = function () {
+      return false;
+      // return AuthenticationService.isLoggedIn();
+    };
+    $scope.login = function (credentials) {
+      this.credentials = credentials;
+      // AuthenticationService.login({ credentials: credentials }).then(function (user) {
+      //   $rootScope.$broadcast('auth', AUTH_EVENTS.loginSuccess);
+      //   $scope.currentUser = user;
+      //   $location.path('/');
+      // }, function () {
+      //   $rootScope.$broadcast('auth', AUTH_EVENTS.loginFailed);
+      // });
+    };
+  });
