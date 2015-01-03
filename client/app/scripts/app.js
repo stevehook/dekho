49014185722fab3a53eab65a6de136e8315f333c
Steve Hook <steve.hook@gmail.com>
2015-01-03 20:53:49 +0000

    Add authentication token to API requests

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -8,12 +8,30 @@ angular
     'ngStorage',
     'ui.router'
   ])
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, $httpProvider) {
     // $urlRouterProvider.otherwise('/login');
     $stateProvider.state('login', {
       url: '/login',
       templateUrl: 'app/views/login.html',
       controller: 'Login'
     });
+
+    $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
+      return {
+        'request': function (config) {
+          config.headers = config.headers || {};
+          if ($localStorage.token) {
+            config.headers.Authorization = 'Bearer ' + $localStorage.token;
+          }
+          return config;
+        },
+        'responseError': function(response) {
+          if(response.status === 401 || response.status === 403) {
+            $location.path('/signin');
+          }
+          return $q.reject(response);
+        }
+      };
+    }]);
   });
 
