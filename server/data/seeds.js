e0f73beb84f26f61cd53ae1157bd3714beb4e419
Steve Hook <steve.hook@gmail.com>
2015-01-21 20:57:28 +0000

    Add decks to seeds.js
    Getting messy need to sort out those callbacks

--- server/data/seeds.js
+++ server/data/seeds.js
@@ -1,12 +1,30 @@
 'use strict';
 
-var db = require('../models');
+var db = require('../models'),
+    _ = require('lodash');
 
 var userFixture = { email: 'bob@example.com', name: 'Bob Roberts', password: 'secret' };
+var deckFixtures = [
+      { title: 'Grunt for beginners', synopsis: 'A short presentation about Grunt' },
+      { title: 'Bower for beginners', synopsis: 'A short presentation about Bower' },
+      { title: 'Node.js for beginners', synopsis: 'A short presentation about Node.js' }
+    ];
 
-var user = db.User.build(userFixture);
-user.setPassword('secret', function() {
-  user.save().then(function() {
-    process.exit();
+db.Deck.destroy({}).then(function() {
+  db.User.destroy({}).then(function() {
+
+    var user = db.User.build(userFixture);
+    user.setPassword('secret', function() {
+      user.save().then(function() {
+        _.each(deckFixtures, function(deck) {
+          deck.userId = user.id;
+        });
+
+        var first = db.Deck.build(deckFixtures[0]);
+        first.save().then(function() {
+          process.exit();
+        });
+      });
+    });
   });
 });
