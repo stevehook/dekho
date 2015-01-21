ec4233b0ee6198779776d4c6863f81da05338472
Steve Hook <steve.hook@gmail.com>
2015-01-21 21:13:45 +0000

    Tidy up seeds.js using async

--- server/data/seeds.js
+++ server/data/seeds.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var db = require('../models'),
+    async = require('async'),
     _ = require('lodash');
 
 var userFixture = { email: 'bob@example.com', name: 'Bob Roberts', password: 'secret' };
@@ -9,22 +10,25 @@ var deckFixtures = [
       { title: 'Bower for beginners', synopsis: 'A short presentation about Bower' },
       { title: 'Node.js for beginners', synopsis: 'A short presentation about Node.js' }
     ];
+var user;
 
-db.Deck.destroy({}).then(function() {
-  db.User.destroy({}).then(function() {
-
-    var user = db.User.build(userFixture);
+async.series([
+  function(callback) { db.Deck.destroy({}).then(function() { callback(null); }); },
+  function(callback) { db.Token.destroy({}).then(function() { callback(null); }); },
+  function(callback) { db.User.destroy({}).then(function() { callback(null); }); },
+  function(callback) {
+    user = db.User.build(userFixture);
     user.setPassword('secret', function() {
-      user.save().then(function() {
-        _.each(deckFixtures, function(deck) {
-          deck.userId = user.id;
-        });
-
-        var first = db.Deck.build(deckFixtures[0]);
-        first.save().then(function() {
-          process.exit();
-        });
-      });
+      user.save().then(function() { callback(null); });
     });
-  });
+  },
+  function(callback) {
+    _.each(deckFixtures, function(deck) {
+      deck.userId = user.id;
+    });
+
+    db.Deck.bulkCreate(deckFixtures).then(function() { callback(null); });
+  }
+], function() {
+  process.exit();
 });
