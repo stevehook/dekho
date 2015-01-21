228261ed72f0d43ae7e011067b2b79d16bdce52c
Steve Hook <steve.hook@gmail.com>
2015-01-21 20:45:08 +0000

    Hook up controller to service to call GET decks API

--- client/app/scripts/controllers/home.js
+++ client/app/scripts/controllers/home.js
@@ -2,10 +2,8 @@
 'use strict';
 
 angular.module('dekho')
-  .controller('Home', function($scope) {
-    $scope.decks = [
-      { title: 'Grunt for beginners', synopsis: 'A short presentation about Grunt' },
-      { title: 'Bower for beginners', synopsis: 'A short presentation about Bower' },
-      { title: 'Node.js for beginners', synopsis: 'A short presentation about Node.js' }
-    ];
+  .controller('Home', function($scope, Decks) {
+    Decks.index().then(function(result) {
+      $scope.decks = result.data;
+    });
   });
