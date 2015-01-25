5df75f5e735fd03ec4053d223e56f6c92b02a454
Steve Hook <steve.hook@gmail.com>
2015-01-25 13:26:35 +0000

    Adds outline of new deck dialog

--- client/app/scripts/controllers/home.js
+++ client/app/scripts/controllers/home.js
@@ -2,8 +2,11 @@
 'use strict';
 
 angular.module('dekho')
-  .controller('Home', function($scope, Decks) {
+  .controller('Home', function($scope, $modal, Decks) {
     Decks.index().then(function(result) {
       $scope.decks = result.data;
     });
+    $scope.newDeck = function() {
+      $modal({title: 'New Deck', contentTemplate: 'app/views/modals/newDeck.html', show: true});
+    };
   });
