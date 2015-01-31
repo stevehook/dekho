7d130b1eaa1fb7cffa40988f0ecc2f7d7363a2ec
Steve Hook <steve.hook@gmail.com>
2015-01-31 17:30:36 +0000

    Adds specs for the home controller

--- client/app/scripts/services/decks.js
+++ client/app/scripts/services/decks.js
@@ -1,18 +1,19 @@
 'use strict';
 
 angular.module('dekho')
-  .service('Decks', function ($http, $rootScope, DECK_EVENTS) {
+  .service('Decks', function ($http, $rootScope, $q, DECK_EVENTS) {
     this.index = function() {
       return $http.get('/decks');
     };
     this.create = function(newDeck) {
-      return $http.post('/decks', newDeck, { headers: { 'X-Http-Method-Override': 'PATCH' } }).
+      var deferred = $http.post('/decks', newDeck, { headers: { 'X-Http-Method-Override': 'PATCH' } }).
         success(function(deck) {
           $rootScope.$broadcast('deck', DECK_EVENTS.createSuccess, deck);
         }).
         error(function() {
           $rootScope.$broadcast('deck', DECK_EVENTS.createFailed);
         });
+      return $q.when(deferred);
     };
   });
 
