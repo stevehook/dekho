cd203dc355ae3295a7bf16d9b33249c201906ec7
Steve Hook <steve.hook@gmail.com>
2015-02-15 19:25:31 +0000

    Adds footer with buttons to mode forward/back etc.

--- client/app/scripts/directives/slideshow.js
+++ client/app/scripts/directives/slideshow.js
@@ -8,6 +8,14 @@ angular.module('dekho').directive('slideshow', function($window) {
     },
     template: '<div class="slideshow">' +
               '<div ng-repeat="slide in deck.slides" ng-class="{visible: $first, slide: true}">{{slide.content}}</div>' +
+              '<div class="footer">' +
+              '<span class="glyphicon glyphicon-fast-backward" ng-click="showFirst()"></span>' +
+              '<span class="glyphicon glyphicon-backward" ng-click="showPrevious()"></span>' +
+              '<span class="">{{currentSlide + 1}}/{{slideCount()}}</span>' +
+              '<span class="glyphicon glyphicon-forward" ng-click="showNext()"></span>' +
+              '<span class="glyphicon glyphicon-fast-forward" ng-click="showLast()"></span>' +
+              '<span class="glyphicon glyphicon-remove" ng-click="closeSlideshow()"></span>' +
+              '</div>' +
               '</div>',
     link: function(scope, elem) {
       scope.currentSlide = 0;
@@ -18,15 +26,35 @@ angular.module('dekho').directive('slideshow', function($window) {
         angular.element(elem.find('.slide')[scope.currentSlide]).addClass('visible');
       };
 
+      scope.slideCount = function() {
+        return scope.deck.slides.length;
+      };
+
+      scope.showFirst = function() {
+        scope.setCurrentSlide(0);
+      };
+
+      scope.showLast = function() {
+        scope.setCurrentSlide(scope.slideCount() - 1);
+      };
+
+      scope.showNext = function() {
+        if (scope.currentSlide < (scope.deck.slides.length - 1)) {
+          scope.setCurrentSlide(scope.currentSlide + 1);
+        }
+      };
+
+      scope.showPrevious = function() {
+        if (scope.currentSlide > 0) {
+          scope.setCurrentSlide(scope.currentSlide - 1);
+        }
+      };
+
       angular.element($window).on('keydown', function(e) {
         if (e.keyCode === 37) {
-          if (scope.currentSlide > 0) {
-            scope.setCurrentSlide(scope.currentSlide - 1);
-          }
+          scope.showPrevious();
         } else if (e.keyCode === 39) {
-          if (scope.currentSlide < (scope.deck.slides.length - 1)) {
-            scope.setCurrentSlide(scope.currentSlide + 1);
-          }
+          scope.showNext();
         }
       });
     }
